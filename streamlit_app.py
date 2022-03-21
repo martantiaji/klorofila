46c66be10d460ac2679692dce4199efacb1d3f19
Martanti <97380159+martantiaji@users.noreply.github.com>
2022-03-21 22:44:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, geemap_script, upload  # import your app modules here
+from apps import home, geemap_script  # import your app modules here
 
 st.set_page_config(page_title="Streamlit for Geospatial", layout="wide")
 
